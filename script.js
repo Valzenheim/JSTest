9cb669c9eee265fb502b4ecd2f49e913d9f52095
Valzenheim <ingellion@gmail.com>
2020-12-04 23:26:05 +0300

    final

--- script.js
+++ script.js
@@ -1,30 +1,37 @@
 function submitHandler(){
-    let inputArr = document.getElementsByName('one');
-    let inputValue = inputArr[+inputArr.length-1].value;
-    console.log(inputValue)
+    let inputArr = document.getElementsByClassName('one');
+    let inputValue = inputArr[inputArr.length-1].value;
     let elem = document.createElement('input')
-    let parentElem = document.getElementById('myForm');
+    let parentElem = document.getElementById('formList');
     elem.setAttribute('id',inputArr.length+1);
-    elem.setAttribute('name','one')
+    elem.setAttribute('class','one')
     elem.setAttribute('type','text')
-    parentElem.append(elem);
+    parentElem.appendChild(elem).focus();
 
-    if(inputValue.indexOf('555', 0)>=0 && +inputArr.length%2===0) {
-        inputValue = inputValue.replace('555', '---').toUpperCase();
-        alert(inputValue)
-        document.getElementById(inputArr.length + 1).value = inputValue
-        parentElem.reset()
-        return false;
-    } else if(inputValue.indexOf('555', 0)>=0 && !+inputArr.length%2===0) {
-        inputValue = inputValue.replace('555', '---').toLowerCase();
-        document.getElementById(inputArr.length+1).value = inputValue
-        parentElem.reset()
-        return false;
-    } else if(+inputArr.length%2===0){
+    if(inputValue.indexOf('555', 0)>=0){
+        if(inputArr.length%2===0){
+            inputValue = inputValue.replace('555', '---').toUpperCase();
+            document.getElementById(inputArr.length).value = inputValue;
+            console.log(inputValue)
+            return false;
+        }else{
+            inputValue = inputValue.replace('555', '---').toLowerCase();
+            document.getElementById(inputArr.length).value = inputValue;
+            console.log(inputValue)
+            return false;
+        }
+    }else if(inputArr.length%2===0){
         inputValue = inputValue.toUpperCase();
-        document.getElementById(inputArr.length+1).value = inputValue
-        parentElem.reset()
+        document.getElementById(inputArr.length).value = inputValue;
+        console.log(inputValue)
+        return false;
+    }else{
+        inputValue = inputValue.toLowerCase();
+        document.getElementById(inputArr.length).value = inputValue;
+        console.log(inputValue)
         return false;
     }
+
+
 
 }
