6d22efbc0adc58d8deede19c44c701aec48b6f17
Michael Yaruta <ingellion@gmail.com>
2020-12-09 15:18:48 +0300

    complete

--- script.js
+++ script.js
@@ -3,35 +3,23 @@ function submitHandler(){
     let inputValue = inputArr[inputArr.length-1].value;
     let elem = document.createElement('input')
     let parentElem = document.getElementById('formList');
+
     elem.setAttribute('id',inputArr.length+1);
     elem.setAttribute('class','one')
     elem.setAttribute('type','text')
+
     parentElem.appendChild(elem).focus();
 
-    if(inputValue.indexOf('555', 0)>=0){
-        if(inputArr.length%2===0){
-            inputValue = inputValue.replace('555', '---').toUpperCase();
-            document.getElementById(inputArr.length).value = inputValue;
-            console.log(inputValue)
-            return false;
-        }else{
-            inputValue = inputValue.replace('555', '---').toLowerCase();
-            document.getElementById(inputArr.length).value = inputValue;
-            console.log(inputValue)
-            return false;
-        }
-    }else if(inputArr.length%2===0){
-        inputValue = inputValue.toUpperCase();
-        document.getElementById(inputArr.length).value = inputValue;
-        console.log(inputValue)
-        return false;
-    }else{
-        inputValue = inputValue.toLowerCase();
-        document.getElementById(inputArr.length).value = inputValue;
-        console.log(inputValue)
-        return false;
+    let addValue = document.getElementById(inputArr.length);
+
+    if(inputValue.indexOf('555', 0)>=0 && inputArr.length % 2 === 0) {
+       addValue.value = inputValue.replace('555', '---').toUpperCase();
+    }else if(inputValue.indexOf('555', 0)>=0 && inputArr.length % 2 !== 0){
+        addValue.value = inputValue.replace('555', '---').toLowerCase()
+    }else if (inputArr.length % 2 === 0) {
+        addValue.value = inputValue.toUpperCase();
+    }else if(inputArr.length % 2 !== 0){
+        addValue.value = inputValue.toLowerCase();
     }
-
-
-
+    return false
 }
